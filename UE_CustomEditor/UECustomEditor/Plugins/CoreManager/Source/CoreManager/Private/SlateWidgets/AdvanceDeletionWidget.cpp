09a4ed705980be6f2c514132e716656e4cb60173
thebiggestmonstro <monstroowner@gmail.com>
2026-02-15 17:17:41 +0900

    Add SCheckBox

--- UE_CustomEditor/UECustomEditor/Plugins/CoreManager/Source/CoreManager/Private/SlateWidgets/AdvanceDeletionWidget.cpp
+++ UE_CustomEditor/UECustomEditor/Plugins/CoreManager/Source/CoreManager/Private/SlateWidgets/AdvanceDeletionWidget.cpp
@@ -1,5 +1,6 @@
 #include "SlateWidgets/AdvanceDeletionWidget.h"
 #include "SlateBasics.h"
+#include "DebugHeader.h"
 
 void SAdvanceDeletionTab::Construct(const FArguments& InArgs)
 {
@@ -14,7 +15,7 @@ void SAdvanceDeletionTab::Construct(const FArguments& InArgs)
 			SNew(SVerticalBox)
 
 				//First vertical slot for title text
-				+ SVerticalBox::Slot()
+				+SVerticalBox::Slot()
 				.AutoHeight()
 				[
 					SNew(STextBlock)
@@ -24,20 +25,20 @@ void SAdvanceDeletionTab::Construct(const FArguments& InArgs)
 						.ColorAndOpacity(FColor::White)
 				]
 
-			//SecondSlot for drop down to specify the listing condition and help text
-			+ SVerticalBox::Slot()
+				//SecondSlot for drop down to specify the listing condition and help text
+				+SVerticalBox::Slot()
 				.AutoHeight()
 				[
 					SNew(SHorizontalBox)
 				]
 
 				//Third slot for the asset list
-				+ SVerticalBox::Slot()
-				.AutoHeight()
+				+SVerticalBox::Slot()
+				.VAlign(VAlign_Fill)
 				[
 					SNew(SScrollBox)
 
-						+ SScrollBox::Slot()
+						+SScrollBox::Slot()
 						[
 							SNew(SListView<TSharedPtr<FAssetData>>)
 								.ItemHeight(24.f)
@@ -46,8 +47,8 @@ void SAdvanceDeletionTab::Construct(const FArguments& InArgs)
 						]
 				]
 
-			//Fourth slot for 3 buttons
-			+ SVerticalBox::Slot()
+				//Fourth slot for 3 buttons
+				+SVerticalBox::Slot()
 				.AutoHeight()
 				[
 					SNew(SHorizontalBox)
@@ -57,14 +58,87 @@ void SAdvanceDeletionTab::Construct(const FArguments& InArgs)
 
 TSharedRef<ITableRow> SAdvanceDeletionTab::OnGenerateRowForList(TSharedPtr<FAssetData> AssetDataToDisplay, const TSharedRef<STableViewBase>& OwnerTable)
 {
+	if (!AssetDataToDisplay.IsValid()) 
+	{
+		return SNew(STableRow<TSharedPtr<FAssetData>>, OwnerTable);
+	}
+
+	const FString DisplayAssetClassName = AssetDataToDisplay->AssetClassPath.GetAssetName().ToString();
 	const FString DisplayAssetName = AssetDataToDisplay->AssetName.ToString();
 
-	TSharedRef<STableRow<SharedPtr<FAssetData>>> ListViewRowWidget =
+	FSlateFontInfo AssetClassNameFont = GetEmboseedTextFont();
+	AssetClassNameFont.Size = 10;
+
+	FSlateFontInfo AssetNameFont = GetEmboseedTextFont();
+	AssetNameFont.Size = 15;
+
+	TSharedRef<STableRow<TSharedPtr<FAssetData>>> ListViewRowWidget =
 		SNew(STableRow<TSharedPtr<FAssetData>>, OwnerTable)
 		[
-			SNew(STextBlock)
-				.Text(FText::FromString(DisplayAssetName))
+			SNew(SHorizontalBox)
+				//First slot for check box
+				+SHorizontalBox::Slot()
+				.HAlign(HAlign_Left)
+				.VAlign(VAlign_Center)
+				.FillWidth(.05f)
+				[
+					ConstructCheckBox(AssetDataToDisplay)
+				]
+
+				//Second slot for displaying asset class name
+				+ SHorizontalBox::Slot()
+				.HAlign(HAlign_Center)
+				.VAlign(VAlign_Fill)
+				.FillWidth(.2f)
+				[
+					ConstructTextForRowWidget(DisplayAssetClassName, AssetClassNameFont)
+				]
+
+				//Third slot for displaying asset name
+				+SHorizontalBox::Slot()
+				[
+					ConstructTextForRowWidget(DisplayAssetName, AssetNameFont)
+				]
+
+				//Fourth slot for a button
 		];
 
 	return ListViewRowWidget;
+}
+
+TSharedRef<SCheckBox> SAdvanceDeletionTab::ConstructCheckBox(const TSharedPtr<FAssetData>& AssetDataToDisplay)
+{
+	TSharedRef<SCheckBox> ConstructedCheckBox = SNew(SCheckBox)
+		.Type(ESlateCheckBoxType::CheckBox)
+		.OnCheckStateChanged(this, &SAdvanceDeletionTab::OnCheckBoxStateChanged, AssetDataToDisplay)
+		.Visibility(EVisibility::Visible);
+
+	return ConstructedCheckBox;
+}
+
+void SAdvanceDeletionTab::OnCheckBoxStateChanged(ECheckBoxState NewState, TSharedPtr<FAssetData> AssetData)
+{
+	switch (NewState)
+	{
+	case ECheckBoxState::Unchecked:
+		DebugHeader::Print(AssetData->AssetName.ToString() + TEXT(" is unchecked"), FColor::Red);
+		break;
+	case ECheckBoxState::Checked:
+		DebugHeader::Print(AssetData->AssetName.ToString() + TEXT(" is checked"), FColor::Green);
+		break;
+	case ECheckBoxState::Undetermined:
+		break;
+	default:
+		break;
+	}
+}
+
+TSharedRef<STextBlock> SAdvanceDeletionTab::ConstructTextForRowWidget(const FString& TextContent, const FSlateFontInfo& FontToUse)
+{
+	TSharedRef<STextBlock> ConstructedTextBlock = SNew(STextBlock)
+		.Text(FText::FromString(TextContent))
+		.Font(FontToUse)
+		.ColorAndOpacity(FColor::White);
+
+	return ConstructedTextBlock;
 }
--- UE_CustomEditor/UECustomEditor/Plugins/CoreManager/Source/CoreManager/Public/SlateWidgets/AdvanceDeletionWidget.h
+++ UE_CustomEditor/UECustomEditor/Plugins/CoreManager/Source/CoreManager/Public/SlateWidgets/AdvanceDeletionWidget.h
@@ -16,4 +16,8 @@ public:
 private:
 	TArray<TSharedPtr<FAssetData>> StoredAssetsData;
 	TSharedRef<ITableRow> OnGenerateRowForList(TSharedPtr<FAssetData> AssetDataToDisplay, const TSharedRef<STableViewBase>& OwnerTable);
+	TSharedRef<SCheckBox> ConstructCheckBox(const TSharedPtr<FAssetData>& AssetDataToDisplay);
+	void OnCheckBoxStateChanged(ECheckBoxState NewState, TSharedPtr<FAssetData> AssetData);
+	TSharedRef<STextBlock> ConstructTextForRowWidget(const FString& TextContent, const FSlateFontInfo& FontToUse);
+	FSlateFontInfo GetEmboseedTextFont() const { return FCoreStyle::Get().GetFontStyle(FName("EmbossedText")); }
 };
