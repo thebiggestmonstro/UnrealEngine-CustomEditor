23e2e7eb73590850bbc29f5f71f489ae1eaaf6ec
thebiggestmonstro <monstroowner@gmail.com>
2026-03-06 11:55:43 +0900

    Use EditorActorSubsystem

---
+++ UE_CustomEditor/UECustomEditor/Plugins/CoreManager/Source/CoreManager/Public/ActorActions/QuickActorActionsWidget.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorUtilityWidget.h"
+#include "QuickActorActionsWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COREMANAGER_API UQuickActorActionsWidget : public UEditorUtilityWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	void SelectAllActorsWithSimilarName();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ActorBatchSelection")
+	TEnumAsByte<ESearchCase::Type> SearchCase = ESearchCase::IgnoreCase;
+
+private:
+	UPROPERTY()
+	class UEditorActorSubsystem* EditorActorSubsystem;
+
+	bool GetEditorActorSubsystem();
+};
